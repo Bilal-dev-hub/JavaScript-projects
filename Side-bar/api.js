076fc8747cf021022661fc3c5b62a874939198c0
Bilal-dev-hub <m.bilal00989@gmail.com>
2025-10-01 17:37:53 +0500

    Side-bar Project

---
+++ Side-bar/api.js
@@ -0,0 +1,31 @@
+// const toggleBtn = document.querySelector(".sidebar-toggle");
+// const closeBtn = document.querySelector(".close-btn");
+// const sidebar = document.querySelector(".sidebar");
+
+// toggleBtn.addEventListener("click", function () {
+// if (sidebar.classList.contains("show-sidebar")) {
+//   sidebar.classList.remove("show-sidebar");
+// } else {
+//   sidebar.classList.add("show-sidebar");
+// }
+// sidebar.classList.toggle("show-sidebar");
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   sidebar.classList.remove("show-sidebar");
+// });
+
+
+
+
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sideBar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  sideBar.classList.toggle("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sideBar.classList.remove("show-sidebar");
+});
